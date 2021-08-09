62438210581ecee0cf44275657b6cbe5e289daf7
toliveira90 <34128530+toliveira90@users.noreply.github.com>
2021-08-09 22:45:23 +0100

    Update App.js

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
           </a>{" "}
           and is{" "}
           <a
-            href="https://github.com/wecodeschool/react-weather-app"
+            href="https://github.com/toliveira90"
             target="_blank"
             rel="noopener noreferrer"
           >
